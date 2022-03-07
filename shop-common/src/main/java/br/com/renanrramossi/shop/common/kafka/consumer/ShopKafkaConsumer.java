a459a19477e309098c0753cbbc0da79c2c8037dd
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2022-03-07 13:01:23 -0300

    Incluido novo modulo common

--- shop-common/src/main/java/br/com/renanrramossi/shop/common/kafka/consumer/ShopKafkaConsumer.java
+++ shop-common/src/main/java/br/com/renanrramossi/shop/common/kafka/consumer/ShopKafkaConsumer.java
@@ -1,2 +1,38 @@
-package br.com.renanrramossi.shop.common.kafka.consumer;public class ShopKafkaConsumer {
+package br.com.renanrramossi.shop.common.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ShopKafkaConsumer<T> {
+
+	@Value("${kafka.bootstrapAddress:localhost:9092}")
+	private String bootstrapAddress;
+
+	private ConsumerFactory<String, T> consumerFactory() {
+		final JsonDeserializer<T> deserializer = new JsonDeserializer<>();
+
+		final Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+
+		return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, T> kafkaListenerContainerFactory() {
+		final ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+
 }
