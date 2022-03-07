4581c88a28edcf4e9cea0eb30357c30f2cd12b21
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2022-03-07 14:35:48 -0300

    Alteracao de nome da classe main

--- shop-report/src/main/java/br/com/renanrramossi/shop/Application.java
+++ shop-report/src/main/java/br/com/renanrramossi/shop/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopReportApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopReportApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
