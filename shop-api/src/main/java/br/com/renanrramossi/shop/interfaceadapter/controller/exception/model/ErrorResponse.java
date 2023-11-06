db93b43dd90130f2ad644fc0f0d08bb5bf967d43
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2023-11-06 09:51:36 -0300

    Introduce new obect structures in order to format bad request error response

--- shop-api/src/main/java/br/com/renanrramossi/shop/interfaceadapter/controller/exception/model/ErrorResponse.java
+++ shop-api/src/main/java/br/com/renanrramossi/shop/interfaceadapter/controller/exception/model/ErrorResponse.java
@@ -1,2 +1,45 @@
-package br.com.renanrramossi.shop.interfaceadapter.controller.exception.model;public class ErrorResponse {
+package br.com.renanrramossi.shop.interfaceadapter.controller.exception.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@JsonInclude(Include.NON_NULL)
+@JsonPropertyOrder({
+    "timestamp",
+    "status",
+    "error",
+    "validations"
+})
+public class ErrorResponse {
+
+  @NonNull
+  @Builder.Default
+  @JsonProperty(value = "timestamp", required = true)
+  @JsonFormat(shape = Shape.STRING)
+  Date timestamp = Timestamp.valueOf(LocalDateTime.now(ZoneId.systemDefault()));
+
+  @JsonProperty(value = "status", required = true)
+  Integer status;
+
+  @JsonProperty(value = "error", required = true)
+  String error;
+
+  @JsonProperty("validations")
+  List<FieldErrorValidation> validations;
 }
--- shop-api/src/main/java/br/com/renanrramossi/shop/interfaceadapter/controller/exception/model/FieldErrorValidation.java
+++ shop-api/src/main/java/br/com/renanrramossi/shop/interfaceadapter/controller/exception/model/FieldErrorValidation.java
@@ -1,2 +1,30 @@
-package br.com.renanrramossi.shop.interfaceadapter.controller.exception.model;public class FieldErrorValidation {
+package br.com.renanrramossi.shop.interfaceadapter.controller.exception.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@JsonInclude(Include.NON_NULL)
+@JsonPropertyOrder({
+    "fieldName",
+    "fieldValue",
+    "message"
+})
+public class FieldErrorValidation {
+
+  @JsonProperty(value = "fieldName", required = true)
+  String fieldName;
+
+  @JsonProperty(value = "fieldValue", required = true)
+  Object fieldValue;
+
+  @JsonProperty(value = "message", required = true)
+  String message;
 }
